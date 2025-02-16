ff9924ddd3aa25c62cc862d0a58c61e80fb60659
Douglinhas <daguiaralcantara@gmail.com>
2025-02-16 01:44:26 -0300

    test: test UpdateUserRepository

---
+++ tests/infra/db/repositories/user/update-user.spec.ts
@@ -0,0 +1,42 @@
+import { MongooseHelper } from "@/infra/db/mongoose/mongoose-helper";
+import { UpdateUserMongooseRepository } from "@/infra/db/mongoose/repositories/user";
+import { UserModel } from "@/infra/db/mongoose/schemas";
+import { env } from "@/main/config/env";
+
+let userId: string;
+
+describe("UpdateUserMongooseRepository", () => {
+  beforeAll(async () => {
+    await MongooseHelper.connect(env.mongoUrl);
+    
+    const user = await UserModel.create({
+      name: "John Doe",
+      email: "john@example.com",
+      password: "securePassword123!"
+    });
+    userId = user._id.toString();  
+  });
+
+  afterAll(async () => {
+    await UserModel.deleteMany({});
+    await MongooseHelper.disconnect();
+  });
+
+  it("should update the user", async () => {
+    const repository = new UpdateUserMongooseRepository();
+
+    await repository.handle({
+      id: userId,
+      name: "John Doe",
+      email: "john_doe@example.com",
+      password: "securePassword123!"
+    });
+
+    const updatedUser = await UserModel.findById(userId);
+
+    expect(updatedUser).toBeDefined();
+    expect(updatedUser?.name).toBe("John Doe");
+    expect(updatedUser?.email).toBe("john_doe@example.com");
+    expect(updatedUser?.password).toBe("securePassword123!");
+  });
+});
