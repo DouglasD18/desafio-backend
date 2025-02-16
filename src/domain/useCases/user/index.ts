0dad77240cd8ecc7bc8b2a15738c3f5ac3f317cc
Douglinhas <daguiaralcantara@gmail.com>
2025-02-16 11:22:04 -0300

    feat: create DeleteUserController

--- src/domain/useCases/user/index.ts
+++ src/domain/useCases/user/index.ts
@@ -2,3 +2,4 @@ export * from "./create-user";
 export * from "./validator";
 export * from "./list-users";
 export * from "./update-user";
+export * from "./delete-user";
---
+++ src/presentation/controllers/user/delete-user.ts
@@ -0,0 +1,38 @@
+import { DeleteUser, Validator } from "../../../domain/useCases/user";
+import { badRequest, internalServerError, noContent, notFound } from "../../helpers/http";
+import { Controller, HttpRequest, HttpResponse } from "../../protocols";
+
+export class DeleteUserController implements Controller {
+  private readonly deleteUser: DeleteUser;
+  private readonly validator: Validator;
+
+  constructor(deleteUser: DeleteUser, validator: Validator) {
+    this.deleteUser = deleteUser;
+    this.validator = validator;
+  }
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const id = httpRequest.params.id;
+    
+    const validated = this.validator.handle(id);
+    if (!validated.isValid) {
+      return badRequest(validated.message!)
+    }
+
+    try {
+      const result = await this.deleteUser.handle(id);
+      if (!result) {
+        return notFound("Usuário não encontrado");
+      }
+      
+      return noContent();
+    } catch (error: unknown) { 
+      if (error instanceof Error) { 
+        return internalServerError(error.message);
+      }
+      
+      return internalServerError("Erro desconhecido");
+    }
+  }
+  
+}
