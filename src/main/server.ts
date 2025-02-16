93e476eb15f18dfe3eb5cee3feee89c0644d4d15
Douglinhas <daguiaralcantara@gmail.com>
2025-02-15 23:06:57 -0300

    feat: conect and disconect mongo in server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,29 @@
+import { MongooseHelper } from "../infra/db/mongoose/mongoose-helper";
 import app from "./config/app";
 import { env } from "./config/env";
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the API!");
+});
+
 app.listen(env.port, async () => {
-  console.log("Server is Running!");
+  try {
+    await MongooseHelper.connect(env.mongoUrl);
+    
+    console.log("Server is Running!");
+  } catch (error) {
+    console.error("Erro ao iniciar o servidor:", error);
+  }
+});
+
+process.on("SIGINT", async () => {
+  await MongooseHelper.disconnect(); 
+  console.log("Servidor finalizado.");
+  process.exit(0); 
+});
+
+process.on("SIGTERM", async () => {
+  await MongooseHelper.disconnect(); 
+  console.log("Servidor finalizado.");
+  process.exit(0); 
 });
