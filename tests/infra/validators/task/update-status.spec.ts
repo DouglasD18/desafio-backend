b0d67a6df4b32e6cb5be32d71c56e0943490bf54
Douglinhas <daguiaralcantara@gmail.com>
2025-02-17 15:20:53 -0300

    test: test UpdateStatusZodValidator

---
+++ tests/infra/validators/task/update-status.spec.ts
@@ -0,0 +1,49 @@
+import { UpdateStatusZodValidator } from "@/infra/validators/task";
+import { Validated } from "@/domain/models/validated";
+import { TaskStatus } from "@/domain/models/task";
+
+const PAYLOAD = {
+  userId: "64f8eaa4b5b6c0b15a8e4e91", 
+  status: TaskStatus.PENDING
+};
+
+describe("UpdateStatusZodValidator", () => {
+  let validator: UpdateStatusZodValidator;
+
+  beforeEach(() => {
+    validator = new UpdateStatusZodValidator();
+  });
+
+  it("Should return isValid false if userId is invalid", () => {
+    const invalidBody = {
+      ...PAYLOAD,
+      userId: "invalid_id"
+    };
+
+    const result: Validated = validator.handle(invalidBody);
+
+    expect(result.isValid).toBe(false);
+    expect(result.message).toContain("Id inválido");
+  });
+
+  it("Should return isValid false if status is invalid", () => {
+    const invalidBody = {
+      ...PAYLOAD,
+      status: "invalid_status"
+    };
+
+    const result: Validated = validator.handle(invalidBody);
+
+    expect(result.isValid).toBe(false);
+    expect(result.message).toContain("Status inválido");
+  });
+
+  it("Should return isValid true if input is valid", () => {
+    const validBody = PAYLOAD;
+
+    const result: Validated = validator.handle(validBody);
+
+    expect(result.isValid).toBe(true);
+    expect(result.message).toBeUndefined();
+  });
+});
