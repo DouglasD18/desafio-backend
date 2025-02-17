0bb7c7fb2be21dbf5cefbf1887633228d765ff3c
Douglinhas <daguiaralcantara@gmail.com>
2025-02-17 16:08:34 -0300

    feat: create ListTasksByUserRepository

---
+++ src/infra/db/mongoose/repositories/task/list-tasks-by-user.ts
@@ -0,0 +1,17 @@
+import { TaskByUser } from "../../../../../domain/models/task";
+import { ListTasksByUserRepository } from "../../../../../data/protocols/task";
+import { TaskModel } from "../../schemas";
+import { TaskStatus } from "../../../../../domain/models/task"; 
+
+export class ListTaskByIdMongooseRepository implements ListTasksByUserRepository {
+  async handle(id: string): Promise<TaskByUser[]> {
+    const tasks = await TaskModel.find({ userId: id });
+
+    return tasks.map(task => ({
+      id: task._id.toString(),
+      title: task.title,
+      description: task.description,
+      status: task.status as TaskStatus 
+    }));
+  }
+}
