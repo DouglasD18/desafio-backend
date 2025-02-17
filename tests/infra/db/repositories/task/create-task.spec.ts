58986557639d4b861d8857a3406b7754206eabb6
Douglinhas <daguiaralcantara@gmail.com>
2025-02-16 22:38:18 -0300

    test: test CreateTaskRepository

---
+++ tests/infra/db/repositories/task/create-task.spec.ts
@@ -0,0 +1,36 @@
+import { MongooseHelper } from "@/infra/db/mongoose/mongoose-helper";
+import { CreateTaskMongooseRepository } from "@/infra/db/mongoose/repositories/task";
+import { TaskModel, UserModel } from "@/infra/db/mongoose/schemas";
+import { TaskStatus } from "@/domain/models/task";
+import { env } from "@/main/config/env";
+
+describe("CreateTaskMongooseRepository", () => {
+  beforeAll(async () => {
+    await MongooseHelper.connect(env.mongoUrl);
+  });
+
+  afterAll(async () => {
+    await TaskModel.deleteMany({});
+    await UserModel.deleteMany({});
+    await MongooseHelper.disconnect();
+  });
+
+  it("should create a new Task", async () => {
+    const repository = new CreateTaskMongooseRepository();
+    
+    const user = await UserModel.create({
+      name: "John Doe",
+      email: "john@example.com",
+      password: "securePassword123!"
+    });
+
+    const taskId = await repository.handle({
+      userId: user._id.toString(), 
+      title: "valid_title",
+      description: "A description",
+      status: TaskStatus.PENDING
+    });
+
+    expect(taskId).toBeDefined();
+  });
+});
